d9a40a5326ee9251736fc8051e56430dd1d85625
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2025-05-26 23:55:55 +0000

    Feat: create_project

--- src/repositories/project_repository.rs
+++ src/repositories/project_repository.rs
@@ -16,5 +16,23 @@ impl ProjectRepository {
         .await?;
 
         Ok(projects)
-    }
+    },
+    pub async fn create(pool: &PgPool, project: &Project) -> Result<Project, sqlx::Error> {
+    let inserted_project = sqlx::query_as!(
+        Project,
+        r#"
+        INSERT INTO project (id,project_index, title, icon)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, project_index, title, icon
+        "#,
+        project.id
+        project.project_index,
+        project.title,
+        project.icon,
+    )
+    .fetch_one(pool)
+    .await?;
+
+    Ok(inserted_project)
+}
 }
--- src/services/project_service.rs
+++ src/services/project_service.rs
@@ -2,12 +2,15 @@ use crate::models::project_model::Project;
 use crate::repositories::project_repository::ProjectRepository;
 use sqlx::PgPool;
 
+
 pub struct ProjectService;
 
 impl ProjectService {
     pub async fn get_all_projects(pool: &PgPool) -> Result<Vec<Project>, sqlx::Error> {
         ProjectRepository::find_all(pool).await
     }
-
+    pub async fn create_project(pool: &PgPool,project: &Project ) -> Result<Project, sqlx::Error> {
+        ProjectRepository::create(pool, project).await
+    }
    
 }
